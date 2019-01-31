93fb8745bf214d31f4e5c21efb4e88c60e024811
Tomáš Tížek <tomas.tizek@firma.seznam.cz>
2019-01-31 08:16:33 +0100

    add nethod for sort and move files by types

--- mover.py
+++ mover.py
@@ -13,6 +13,8 @@ def parse_args():
     arg_parser.add_argument("--type", help="type of files, which should be moved")
     arg_parser.add_argument("--source", help="source path")
     arg_parser.add_argument("--dest", help="destination path")
+    arg_parser.add_argument("--operation", help="all - sort files by type and move it to corresponding folder,"
+                                                "one - sort files with entered type and move in to destination folder")
     return arg_parser.parse_args()
 
 
@@ -28,6 +30,23 @@ def move_file(type, source, dest):
     print("{} files was moved successfully".format(type))
 
 
+def sort_and_move(source, dest):
+
+    list_of_dirs = []
+
+    for d in os.listdir(source):
+        list_of_dirs.append(d.split(",")[-1].split(".")[1])
+    for dir in list_of_dirs:
+        try:
+            os.makedirs("{}/{}s".format(dest, dir))
+        except:
+            print("folder allready exist")
+
+        for f in tqdm(glob.glob("{}/*{}".format(source,dir))):
+            shutil.move(f, "{}/{}s".format(dest, dir))
+        print("files was moved successfully")
+
+
 def main():
 
     args = parse_args()
@@ -35,8 +54,14 @@ def main():
     type = args.type
     source = args.source
     dest = args.dest
+    operation = args.operation
 
-    move_file(type, source, dest)
+    if operation =="all":
+        sort_and_move(source, dest)
+    elif operation == "one":
+        move_file(type, source, dest)
+    else:
+        print("wrong operation")
 
 
 if __name__ == "__main__":
